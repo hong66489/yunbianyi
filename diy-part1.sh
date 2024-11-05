b29daae0b0385e6544383830dd7b5970b25578fc
hong66489 <483242964@qq.com>
2024-11-05 20:41:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-git clone https://github.com/x-wrt/com.x-wrt.git package/com.x-wrt
+#git clone https://github.com/x-wrt/com.x-wrt.git package/com.x-wrt
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default

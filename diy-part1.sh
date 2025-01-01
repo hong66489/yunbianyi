8c55c88a86fea34005bfb4d117a2adedc6b575a4
hong66489 <483242964@qq.com>
2025-01-01 19:52:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,8 @@
 #
 
 # Uncomment a feed source
-git clone https://github.com/kenzok8/small.git package/small
-git clone https://github.com/kenzok78/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/kenzok8/small-package.git package/small
+#git clone https://github.com/kenzok78/luci-app-adguardhome package/luci-app-adguardhome
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default

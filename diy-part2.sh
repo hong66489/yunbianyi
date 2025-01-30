70de32e6303068df3baf0161665c0de2b117b153
hong66489 <483242964@qq.com>
2025-01-30 21:29:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,7 +18,9 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 # Modify hostname
 sed -i 's/OpenWrt/R619AC/g' package/base-files/files/bin/config_generate
-#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.defaut
+
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {

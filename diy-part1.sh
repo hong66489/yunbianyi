23aeed7f249553ee17a498b905562afe5976d94f
hong66489 <483242964@qq.com>
2025-08-24 22:23:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,7 @@
 # Uncomment a feed source
 #git clone https://github.com/kenzok8/openwrt-packages.git package/small
 #git clone https://github.com/kenzok8/small.git package/kenzok8
-git clone https://github.com/kenzok78/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/xiaoxiao29/luci-app-adguardhome package/luci-app-adguardhome
 #git clone https://github.com/x-wrt/com.x-wrt.git package/com.x-wrt
 # Add a feed source
 

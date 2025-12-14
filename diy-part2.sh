14af6b9a3487902f42e71ff46ae0abe5f190f758
hong66489 <483242964@qq.com>
2025-12-14 16:01:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,7 +18,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 # Modify hostname
 sed -i 's/ImmortalWrt/T6GS/g' package/base-files/files/bin/config_generate
-
+sed -i 's/FCJ G-AX1800-F/360 T6GS/g' target/linux/ramips/dts/mt7621_fcj_g-ax1800-f.dts
 # UPnP
 #rm -rf feeds/{packages/net/miniupnpd,luci/applications/luci-app-upnp}
 #git clone https://git.kejizero.online/zhao/miniupnpd feeds/packages/net/miniupnpd -b v2.3.7

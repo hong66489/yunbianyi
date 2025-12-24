5502740392ff7594b31175862ba2f64e1241e314
hong66489 <483242964@qq.com>
2025-12-24 22:22:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,9 +17,9 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/LEDE/T6GS/g' package/base-files/files/bin/config_generate
-sed -i 's/FCJ G-AX1800-F/360 T6GS/g' target/linux/ramips/dts/mt7621_fcj_g-ax1800-f.dts
-sed -i 's/LEDE/360-T6GS/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/ImmortalWrt/360-T6GS/g' package/base-files/files/bin/config_generate
+#sed -i 's/FCJ G-AX1800-F/360 T6GS/g' target/linux/ramips/dts/mt7621_fcj_g-ax1800-f.dts
+sed -i 's/ImmortalWrt/PandoraBox/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # UPnP
 #rm -rf feeds/{packages/net/miniupnpd,luci/applications/luci-app-upnp}
 #git clone https://git.kejizero.online/zhao/miniupnpd feeds/packages/net/miniupnpd -b v2.3.7

bcd0829c0a640f7894b6497e93cfcc3ebbf2ba78
hong66489 <483242964@qq.com>
2026-01-08 17:45:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 #git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/small2
 #git clone https://github.com/xuanranran/openwrt-package.git package/xuanranran
 #git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
-git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/luci-app-adguardhome
+#git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/luci-app-adguardhome
 #git clone https://github.com/OneNAS-space/luci-app-adguardhome package/luci-app-adguardhome
 #git clone https://github.com/x-wrt/com.x-wrt.git package/com.x-wrt
 # Add a feed source

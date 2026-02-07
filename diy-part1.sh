5e79af82b2e77f56a81a50be625760a1b0f6e481
hong66489 <483242964@qq.com>
2026-02-08 03:13:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,4 +21,4 @@ git clone https://github.com/xuanranran/openwrt-package.git package/xuanranran
 # Add a feed source
 
 #curl -sSL https://raw.githubusercontent.com/hong66489/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
-curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+#curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
